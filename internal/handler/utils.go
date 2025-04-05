8451aa2a586eee2b61eb0d2816977190f8adad77
Rashevsky Vyacheslav <v_rashevsky@boxberry.ru>
2025-04-05 22:20:35 +0300

    almost working middleware

--- internal/handler/utils.go
+++ internal/handler/utils.go
@@ -1,8 +1,9 @@
 package handler
 
 import (
+	"io"
+	"log"
 	"net/http"
-	"net/url"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rashevskiivv/api/internal/entity"
@@ -44,18 +45,32 @@ func HealthCheck(c *gin.Context) {
 
 func TokenAuthMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		client := http.Client{}
-		req := http.Request{Method: http.MethodGet, URL: &url.URL{Host: "localhost", Path: "check"}}
-		req.Header.Set("id", ctx.Request.Header.Get("id"))
-		req.Header.Set("token", ctx.Request.Header.Get("token"))
+		client := http.Client{} // todo Timeout: time.Second * 3
+		req, err := http.NewRequest(http.MethodGet, "http://auth-app/auth/check", nil)
+		req.Header.Add("id", ctx.Request.Header.Get("id"))
+		req.Header.Add("token", ctx.Request.Header.Get("token"))
 
-		resp, err := client.Do(&req)
+		resp, err := client.Do(req)
 		if err != nil {
-			ctx.AbortWithStatus(http.StatusUnauthorized)
+			log.Println(err)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, err)
+			return
+		}
+
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			if err != nil {
+				log.Println(err)
+			}
+		}(resp.Body)
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Println(err)
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, err)
 			return
 		}
 		if resp.StatusCode != http.StatusOK {
-			ctx.AbortWithStatus(resp.StatusCode)
+			ctx.AbortWithStatusJSON(resp.StatusCode, string(body))
 			return
 		}
 
